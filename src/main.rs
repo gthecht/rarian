cfb882b9b89211b31c0e29e9c59330d4be854f13
gthecht <gdhecht@gmail.com>
2023-01-04 23:58:59 +0200

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::path::PathBuf;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread::JoinHandle;
+mod gatherer;
+use crate::gatherer::file_watcher::watch_dir_thread;
+
+fn create_channel() -> (
+    Sender<Result<notify::Event, notify::Error>>,
+    Receiver<Result<notify::Event, notify::Error>>,
+) {
+    return channel();
+}
+
+fn cleanup_thread(thread_ctrl: Sender<bool>, watcher_thread: JoinHandle<()>) {
+    thread_ctrl.send(true).expect("send failed");
+    watcher_thread.join().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = create_channel();
+    let (thread_ctrl, thread_rx) = channel();
+    let path: PathBuf = PathBuf::from("./".to_owned());
+    let watcher_thread = watch_dir_thread(path.as_path(), tx, thread_rx);
+
+    loop {
+        match rx.recv() {
+            Ok(event) => {
+                println!("Hello, {:?}", event);
+            }
+            Err(e) => {
+                println!("rx error: {:?}!", e);
+                break;
+            }
+        }
+    }
+
+    cleanup_thread(thread_ctrl, watcher_thread);
 }
