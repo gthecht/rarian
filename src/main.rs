6b83b5516dc4771041673d9e00ab4fd7a79edeb8
gthecht <gdhecht@gmail.com>
2024-01-12 16:04:27 +0200

    Change main to monitor apps and not files

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use crate::gatherer::file_gatherer::file_gatherer;
 use crate::gatherer::app_gatherer::monitor_processes;
 
 fn main() {
-    file_gatherer("C:/Users/GiladHecht");
     monitor_processes();
+    file_gatherer("C:/Users/GiladHecht");
 }
