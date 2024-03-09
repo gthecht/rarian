87c64a7e24cc0deb0807b70739a4d6681360a83f
gthecht <gdhecht@gmail.com>
2024-03-09 22:47:59 +0200

    Add app module to interact with user

---
+++ src/app.rs
@@ -0,0 +1,5 @@
+pub fn run_app() {
+  // create options for use to pick:
+  // 0. add note
+  // 9. quit
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod gatherer;
+mod app;
 
 use crate::gatherer::app_gatherer::app_gatherer_thread;
 use crate::gatherer::file_gatherer::file_gatherer;
+use crate::app::run_app;
 
 fn main() {
     let file_paths = vec![
@@ -11,6 +13,7 @@ fn main() {
     let log_path = "C:/Users/GiladHecht/workspace/.rarian/";
     let cleanup_file_gatherer = file_gatherer(file_paths, log_path);
     let cleanup_app_gatherer = app_gatherer_thread(log_path);
+    run_app();
     cleanup_app_gatherer();
     cleanup_file_gatherer();
 }
