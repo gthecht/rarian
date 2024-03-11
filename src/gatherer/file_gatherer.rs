9e434293a0e9d16beae59a67c8e838628edd9b85
gthecht <gdhecht@gmail.com>
2024-03-11 11:22:03 +0200

    Refactor file_gatherer

--- src/gatherer/file_gatherer.rs
+++ src/gatherer/file_gatherer.rs
@@ -2,7 +2,6 @@ use serde::Serialize;
 use serde_json;
 extern crate notify;
 use anyhow::{Context, Result};
-use notify::event::*;
 use std::path::PathBuf;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread::{spawn, JoinHandle};
@@ -11,26 +10,23 @@ use std::time::SystemTime;
 use crate::gatherer::file_watcher::watch_dir_thread;
 use crate::gatherer::logger::{FileLogger, Log, LogEvent};
 
-#[derive(Debug, Serialize)]
-enum FileEventKind {
-    Access,
-    Create,
-    Modify,
-    Remove,
+fn create_notify_channel() -> (
+    Sender<Result<notify::Event, notify::Error>>,
+    Receiver<Result<notify::Event, notify::Error>>,
+) {
+    return channel();
 }
 
 #[derive(Debug, Serialize)]
 struct FileEventLog {
-    event_paths: Vec<PathBuf>,
-    event_kind: FileEventKind,
+    event: notify::Event,
     timestamp: SystemTime,
 }
 
 impl FileEventLog {
-    fn new(tupl: (Vec<PathBuf>, FileEventKind)) -> Self {
-        let (event_paths, event_kind) = tupl;
+    fn new(event: notify::Event) -> Self {
         let timestamp = SystemTime::now();
-        FileEventLog { event_paths, event_kind, timestamp }
+        FileEventLog { event, timestamp }
     }
 }
 
@@ -41,41 +37,11 @@ impl LogEvent<FileLogger> for FileEventLog {
     }
 }
 
-fn log_file(
-    rx_event: Result<notify::Event, notify::Error>,
-) -> Result<(Vec<PathBuf>, FileEventKind), notify::Error> {
-    match rx_event {
-        Ok(event) => {
-            let event_paths = event.paths;
-            match event.kind {
-                EventKind::Access(_) => return Ok((event_paths, FileEventKind::Access)),
-                EventKind::Create(_) => return Ok((event_paths, FileEventKind::Create)),
-                EventKind::Modify(_) => return Ok((event_paths, FileEventKind::Modify)),
-                EventKind::Remove(_) => return Ok((event_paths, FileEventKind::Remove)),
-                _ => return Err(notify::Error::generic("event kind not defined")),
-            }
-        }
-        Err(e) => {
-            println!("notify error: {:?}!", e);
-            return Err(e);
-        }
-    }
-}
-
-fn create_notify_channel() -> (
-    Sender<Result<notify::Event, notify::Error>>,
-    Receiver<Result<notify::Event, notify::Error>>,
-) {
-    return channel();
-}
-
-pub fn file_gatherer(file_paths: Vec<String>, log_path: &str) -> impl FnOnce() {
-    let log_path: PathBuf = PathBuf::from(log_path).join("files.json");
-    let mut file_logger = FileLogger::new(log_path);
-
-    let (notify_tx, notify_rx) = create_notify_channel();
-
-    let gatherer_cleanup: Vec<(Sender<bool>, JoinHandle<()>)> = file_paths
+fn create_file_watchers(
+    file_paths: Vec<String>,
+    notify_tx: Sender<Result<notify::Event, notify::Error>>,
+) -> impl FnOnce() {
+    let file_watcher_threads: Vec<(Sender<bool>, JoinHandle<()>)> = file_paths
         .into_iter()
         .map(|file_path| {
             let (notify_ctrl_tx, notify_ctrl_rx) = channel();
@@ -86,17 +52,31 @@ pub fn file_gatherer(file_paths: Vec<String>, log_path: &str) -> impl FnOnce() {
         })
         .collect();
 
+    let close_file_watchers = move || {
+        for (thread_ctrl, watcher_thread) in file_watcher_threads.into_iter() {
+            thread_ctrl.send(true).expect("send failed");
+            watcher_thread.join().unwrap();
+        }
+    };
+    return close_file_watchers;
+}
+
+pub fn file_gatherer(file_paths: Vec<String>, log_path: &str) -> impl FnOnce() {
+    let log_path: PathBuf = PathBuf::from(log_path).join("files.json");
+    let mut file_logger = FileLogger::new(log_path);
+
+    let (notify_tx, notify_rx) = create_notify_channel();
+    let close_file_watchers = create_file_watchers(file_paths, notify_tx);
+
     spawn(move || loop {
         match notify_rx.recv() {
-            Ok(rx_event) => {
-                let log = log_file(rx_event);
-                match log {
-                    Ok(tup) => {
-                        let file_event_log = FileEventLog::new(tup);
-                        file_event_log.log_event(&mut file_logger).expect("log event failed");
-                    },
-                    Err(_e) => break,
-                }
+            Ok(Ok(file_event)) => {
+                let log = FileEventLog::new(file_event);
+                log.log_event(&mut file_logger).expect("log event failed");
+            }
+            Ok(Err(e)) => {
+                println!("notify error: {:?}!", e);
+                break;
             }
             Err(e) => {
                 println!("rx error: {:?}!", e);
@@ -104,10 +84,6 @@ pub fn file_gatherer(file_paths: Vec<String>, log_path: &str) -> impl FnOnce() {
             }
         }
     });
-    return move || {
-        for (thread_ctrl, watcher_thread) in gatherer_cleanup.into_iter() {
-            thread_ctrl.send(true).expect("send failed");
-            watcher_thread.join().unwrap();
-        }
-    };
+
+    return close_file_watchers;
 }
