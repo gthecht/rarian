864bf3f32e0c6de4ff4d1e6c9db7a88f8b820ba0
gthecht <gdhecht@gmail.com>
2024-03-11 07:44:38 +0200

    Create Action struct

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,65 @@
-use console_utils::input::select;
+use std::io;
 
-enum Actions {
-    NewNote,
-    Quit
+#[derive(Debug, Eq, PartialEq)]
+enum ContinueInput {
+    Continue,
+    Break,
+}
+
+struct Action {
+    name: String,
+    func: fn() -> ContinueInput,
+}
+
+impl Action {
+    fn new(name: &str, func: fn() -> ContinueInput) -> Self {
+        Self {
+            name: String::from(name),
+            func: func,
+        }
+    }
+}
+
+fn new_note() -> ContinueInput {
+    println!("enter a new note");
+    return ContinueInput::Continue;
+}
+
+fn quit() -> ContinueInput {
+    println!("quitting");
+    return ContinueInput::Break;
+}
+
+fn build_input_message(actions: &[Action]) -> String {
+    let mut message = String::from("Pick an option:\n");
+    actions.iter().enumerate().for_each(|(index, action)| {
+        message += &format!("{}. {}\n", index, action.name);
+    });
+    message.to_string()
 }
 
 pub fn run_app() {
-  let options = vec!["new note", "quit"];
-  let selected_indices = select("choose action:", &options, false, false);
-  match selected_indices {
-    Some(indices) => println!("selected: {:?}", indices),
-    None => println!("no selected indices")
-  }
+    let actions = vec![
+        Action::new("new note", new_note as fn() -> ContinueInput),
+        Action::new("quit", quit as fn() -> ContinueInput),
+    ];
+    let input_message = build_input_message(&actions);
+    let stdin = io::stdin();
+    let mut run = ContinueInput::Continue;
+    while run == ContinueInput::Continue {
+        let input = &mut String::new();
+        println!("{}", input_message);
+        stdin.read_line(input).expect("failed to read stdin");
+        if let Ok(action_index) = input.trim().parse::<usize>() {
+            match actions.get(action_index) {
+                Some(action) => run = (action.func)(),
+                None => println!(
+                    "please enter an integer corresponding to action: {}",
+                    action_index
+                ),
+            }
+        } else {
+            println!("couldn't parse to usize, try again");
+        }
+    }
 }
