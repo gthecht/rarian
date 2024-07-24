7060cdff3cd8c9d1f68489d0c52661140aefc4d9
gthecht <gdhecht@gmail.com>
2024-07-24 21:25:02 +0300

    Add latest apps window

--- src/app/tui.rs
+++ src/app/tui.rs
@@ -11,14 +11,11 @@ use ratatui::{
         execute,
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     },
-    layout::{Alignment, Rect},
-    style::Stylize,
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::{Modifier, Style, Stylize},
     symbols::border,
     text::{Line, Text},
-    widgets::{
-        block::{Position, Title},
-        Block, Paragraph, Widget,
-    },
+    widgets::{block::Title, Block, List, Paragraph, Widget},
     Frame, Terminal,
 };
 
@@ -41,16 +38,18 @@ pub fn restore() -> io::Result<()> {
 #[derive(Debug)]
 pub struct TuiApp {
     state_machine_tx: Sender<StateMachine>,
-    num: usize,
     exit: bool,
+    notes_window: NotesWindow,
+    last_apps_window: LastAppsWindow,
 }
 
 impl TuiApp {
     pub fn new(state_machine_tx: Sender<StateMachine>, num: usize) -> TuiApp {
         TuiApp {
-            state_machine_tx,
-            num,
+            state_machine_tx: state_machine_tx.clone(),
             exit: false,
+            notes_window: NotesWindow::new(state_machine_tx.clone(), num),
+            last_apps_window: LastAppsWindow::new(state_machine_tx.clone(), num),
         }
     }
 
@@ -63,7 +62,12 @@ impl TuiApp {
     }
 
     fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
+        let layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![Constraint::Percentage(30), Constraint::Percentage(70)])
+            .split(frame.size());
+        frame.render_widget(&self.last_apps_window, layout[0]);
+        frame.render_widget(&self.notes_window, layout[1]);
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -94,21 +98,35 @@ impl TuiApp {
     }
 }
 
-impl Widget for &TuiApp {
+#[derive(Debug)]
+struct NotesWindow {
+    state_machine_tx: Sender<StateMachine>,
+    num: usize,
+}
+
+impl NotesWindow {
+    pub fn new(state_machine_tx: Sender<StateMachine>, num: usize) -> NotesWindow {
+        NotesWindow {
+            state_machine_tx,
+            num,
+        }
+    }
+}
+
+impl Widget for &NotesWindow {
     fn render(self, area: Rect, buf: &mut Buffer) {
         match show_current(self.state_machine_tx.clone(), self.num) {
             Some((title, notes)) => {
+                let title = format!(" {} ", title);
                 let block = Block::bordered()
                     .title(Title::from(title.bold()).alignment(Alignment::Center))
                     .border_set(border::THICK);
 
                 let notes_text = notes
                     .iter()
-                    .map(|note| Line::from(note.text.clone()))
+                    .map(|note| Line::from(format!(" - {}", note.text.clone())))
                     .collect::<Vec<Line>>();
-                Paragraph::new(notes_text)
-                    .block(block)
-                    .render(area, buf);
+                Paragraph::new(notes_text).block(block).render(area, buf);
             }
             None => {
                 let title = Title::from(" no app currently detected ".bold());
@@ -144,30 +162,51 @@ fn show_current(state_machine_tx: Sender<StateMachine>, num: usize) -> Option<(S
     }
 }
 
-fn show_last_apps(state_machine_tx: Sender<StateMachine>, num: usize) {
+#[derive(Debug)]
+struct LastAppsWindow {
+    state_machine_tx: Sender<StateMachine>,
+    num: usize,
+}
+
+impl LastAppsWindow {
+    pub fn new(state_machine_tx: Sender<StateMachine>, num: usize) -> LastAppsWindow {
+        LastAppsWindow {
+            state_machine_tx,
+            num,
+        }
+    }
+}
+
+impl Widget for &LastAppsWindow {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = " latest apps ";
+        let last_apps = show_last_apps(self.state_machine_tx.clone(), self.num);
+        let list = last_apps.iter().map(|app_window| app_window.get_title()).collect::<List>()
+            .block(
+                Block::bordered()
+                    .title(Title::from(title.bold()).alignment(Alignment::Center))
+                    .border_set(border::THICK),
+            )
+            .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
+            .highlight_symbol(">>")
+            .repeat_highlight_symbol(true);
+
+        list.render(area, buf);
+    }
+}
+
+fn show_last_apps(state_machine_tx: Sender<StateMachine>, num: usize) -> Vec<ActiveProcessEvent> {
     let (tx, rx) = channel::<Vec<ActiveProcessEvent>>();
     state_machine_tx
         .send(StateMachine::RecentApps(num, tx))
         .unwrap();
     let last_processes = rx.recv().expect("main thread is alive");
-    println!("last {} apps:", last_processes.len());
-    last_processes.iter().enumerate().for_each(|(index, item)| {
-        println!("{}. {}", index, item.get_title());
-        let (tx, rx) = channel::<Vec<Note>>();
-        state_machine_tx
-            .send(StateMachine::GetAppNotes(item.get_title().to_string(), tx))
-            .unwrap();
-        let app_notes = rx.recv().expect("main thread is alive");
-        app_notes.iter().take(num).for_each(|note| {
-            println!("  - {}", note.text);
-        });
-    });
-    println!();
+    last_processes.into_iter().take(num).collect()
 }
 
 pub fn run_app(state_machine_tx: Sender<StateMachine>) {
     let mut terminal = init().expect("crossterm init failed");
-    let mut tui_app = TuiApp::new(state_machine_tx, 5);
+    let mut tui_app = TuiApp::new(state_machine_tx, 10);
     tui_app.run(&mut terminal).expect("app run failed");
     restore().expect("terminal restore failed");
 }
